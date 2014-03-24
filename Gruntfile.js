0386da24211e584341f85f38f6cbe28be89ea0ff
Valère Colleville <valere.colleville@gmail.com>
2014-03-24 19:39:00 +0100

    change keepAlive to false for test

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
 	protractor: {
     options: {
       configFile: "protractorConfigFile.js", // Default config file
-      //keepAlive: true, // If false, the grunt process stops when the test fails.
+      keepAlive: false, // If false, the grunt process stops when the test fails.
       noColor: false, // If true, protractor will not use colors in its output.
     },
  	test: {}
